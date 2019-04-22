6c90cb672821c9cc1e3bdb4f9128659bbaeafd48
Jakub Wojtyra <wojtyraj@gmail.com>
2019-04-22 15:30:12 +0200

    Main page mobile & desktop, menu

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,41 @@
 import React from "react";
 import { Link } from "gatsby";
+import styled from "styled-components"
+
 import github from "../img/github-icon.svg";
-import logo from "../img/logo.svg";
+import logo_white from "../img/logo_white.png";
+
+const NavStyled = styled.nav`
+  top: 0px;
+  position: fixed;
+  width: 100vw;
+  background-color: #339933;
+  height: 60px;
+  z-index: 100
+`
+
+const NavContainer = styled.div`
+  display: flex;
+  width: 100%;
+  height: 100%;
+  justify-contet: space-between;
+`
+
+const NavBrand = styled.div`
+  display: flex;
+  width: 100%;
+  height: 100%;
+  padding-left: 4%;
+`
+
+const StyledLink = styled(Link)`
+  display: flex;
+  justify-items: center;
+  align-items: center;
+  height: 100%;
+  padding-left: 20px;
+`
+
 
 const Navbar = class extends React.Component {
   constructor(props) {
@@ -34,16 +68,14 @@ const Navbar = class extends React.Component {
 
   render() {
     return (
-      <nav
-        className="navbar is-transparent"
+      <NavStyled
         role="navigation"
-        aria-label="main-navigation"
-      >
-        <div className="container">
-          <div className="navbar-brand">
-            <Link to="/" className="navbar-item" title="Logo">
-              <img src={logo} alt="Kaldi" style={{ width: "88px" }} />
-            </Link>
+        aria-label="main-navigation">
+        <NavContainer>
+          <NavBrand>
+            <StyledLink to="/" title="Logo">
+              <img src={logo_white} alt="Petitet Blanc Logo" style={{ width: "auto", height: '40px' }} />
+            </StyledLink>
             {/* Hamburger menu */}
             <div
               className={`navbar-burger burger ${this.state.navBarActiveClass}`}
@@ -54,43 +86,34 @@ const Navbar = class extends React.Component {
               <span />
               <span />
             </div>
-          </div>
+          </NavBrand>
           <div
             id="navMenu"
             className={`navbar-menu ${this.state.navBarActiveClass}`}
           >
             <div className="navbar-start has-text-centered">
-              <Link className="navbar-item" to="/about">
-                About
+              <Link className="navbar-item" to="/blog">
+                BLOG
               </Link>
               <Link className="navbar-item" to="/products">
-                Products
+                NASZE PSY
               </Link>
-              <Link className="navbar-item" to="/blog">
-                Blog
+              <Link className="navbar-item" to="/about">
+                NASZE MIOTY
               </Link>
               <Link className="navbar-item" to="/contact">
-                Contact
+                GROOMING
               </Link>
               <Link className="navbar-item" to="/contact/examples">
-                Form Examples
+                GALERIA
+              </Link>
+              <Link className="navbar-item" to="/contact/examples">
+                KONTAKT
               </Link>
             </div>
-            <div className="navbar-end has-text-centered">
-              <a
-                className="navbar-item"
-                href="https://github.com/AustinGreen/gatsby-netlify-cms-boilerplate"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <span className="icon">
-                  <img src={github} alt="Github" />
-                </span>
-              </a>
-            </div>
           </div>
-        </div>
-      </nav>
+        </NavContainer>
+      </NavStyled>
     );
   }
 };
--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -1,10 +1,158 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { Link, graphql } from 'gatsby'
+import styled, { keyframes } from "styled-components"
 
 import Layout from '../components/Layout'
 import Features from '../components/Features'
 import BlogRoll from '../components/BlogRoll'
+
+import wallpaper from '../img/wallpaper.jpg'
+import federations from '../img/federations.png'
+
+const WallpaperContainer = styled.div`
+  height: 100vh;
+  width: 100%;
+  overflow: hidden;
+  background-size: cover;
+  position: fixed;
+  top: 50px;
+  background-position: 48%;
+  display: grid;
+  grid-template-rows: 52% 29% 18%;
+  grid-template-areas: "federationsContainer . ." "headerMain headerMain headerMain" " scrollDown scrollDown scrollDown";
+  grid-template-columns: 50% 35% 15%;
+  @media only screen and (min-width: 1088px) {
+    grid-template-rows: 35% 40% 25%;
+  }
+`
+
+const FederationsContainer = styled.div`
+  display: grid;
+  grid-area: federationsContainer;
+  align-self: end;
+  justify-self: center;
+  height: 90px;
+  width: 100%;
+  padding-top: 30px;
+  margin-left: 10px;
+  &:img {
+    background-size: contain;
+    background-repeat: no-repeat;
+  }
+  @media only screen and (min-width: 1088px) {
+    align-items: center;
+    justify-items: start;
+    padding-left: 10vw;
+  }
+`
+
+const ImgFederations = styled.img`
+  width: 200px
+`
+
+const SpanBigSlim = styled.span`
+  position: relative;
+  font-weight: 200;
+  color: #339933;
+  font-size: 1.9em;
+  letter-spacing: 0.9px;
+`
+const SpanStyled = styled.span`
+  letter-spacing: 1px;
+  left: -1px;
+  font-size: 1em;
+  font-weight: ${props => props.weight};
+`
+
+const HeaderMain = styled.h1`
+  font-size: 1.5em;
+  height: auto;
+  line-height: 2em;
+  padding-top: 25px;
+  padding-bottom: 0px;
+  width: 100%;
+  color: #339933;
+  grid-area: headerMain;
+  background: radial-gradient(circle at center,hsla(0,0%,98%,.9),transparent 100%);
+  align-self: end;
+  text-align: center;
+  &:strong {
+    color: #339933 !important;
+  }
+  @media only screen and (min-width: 1088px) {
+    text-align: left;
+    padding-left: 10vw;
+    background: none;
+  }
+`
+
+const scrollDownAnimation = keyframes`
+  0% {
+    transform: translate(0, 0);
+    opacity: 0;
+  }
+  40% {
+    opacity: 1;
+  }
+  80% {
+    transform: translate(0, 20px);
+    opacity: 0;
+  }
+  100% {
+    opacity: 0;
+  }
+}`
+
+const ScrollDown = styled.div`
+  grid-area: scrollDown;
+  position: relative;
+  top: -20px;
+  left: 50%;
+  width: 30px;
+  height: 50px;
+  margin-left: -15px;
+  border: 2px solid #339933;
+  border-radius: 50px;
+  box-sizing: border-box;
+  align-self: center;
+  &:before {
+    position: absolute;
+    top: 10px;
+    left: 50%;
+    content: '';
+    width: 6px;
+    height: 6px;
+    margin-left: -3px;
+    background-color: #339933;
+    border-radius: 100%;
+    -webkit-animation: ${scrollDownAnimation} 2s infinite;
+    animation: ${scrollDownAnimation} 2s infinite;
+    box-sizing: border-box;
+  }
+  &:hover {
+    cursor: pointer;
+  }
+`
+
+const SectionMain = styled.section`
+  position: relative;
+  top: 100vh;
+  width: 100vw;
+  &:before {
+    display: block;
+    content: '';
+    position: relative;
+    top: 50vh;
+    height: 100vh;
+    width: 100vw;
+    background-color: red;
+    background-color: rgba(250,250,250,0.5);
+    backdrop-filter: blur(100px);
+    /* -webkit-backdrop-filter: blur(100px); */
+  }
+
+`
 
 export const IndexPageTemplate = ({
   image,
@@ -16,55 +164,28 @@ export const IndexPageTemplate = ({
   intro,
 }) => (
   <div>
-    <div
-      className="full-width-image margin-top-0"
-      style={{
-        backgroundImage: `url(${
-          !!image.childImageSharp ? image.childImageSharp.fluid.src : image
-        })`,
-        backgroundPosition: `top left`,
-        backgroundAttachment: `fixed`,
-      }}
-    >
-      <div
-        style={{
-          display: 'flex',
-          height: '150px',
-          lineHeight: '1',
-          justifyContent: 'space-around',
-          alignItems: 'left',
-          flexDirection: 'column',
-        }}
-      >
-        <h1
-          className="has-text-weight-bold is-size-3-mobile is-size-2-tablet is-size-1-widescreen"
-          style={{
-            boxShadow:
-              'rgb(255, 68, 0) 0.5rem 0px 0px, rgb(255, 68, 0) -0.5rem 0px 0px',
-            backgroundColor: 'rgb(255, 68, 0)',
-            color: 'white',
-            lineHeight: '1',
-            padding: '0.25em',
-          }}
-        >
-          {title}
-        </h1>
-        <h3
-          className="has-text-weight-bold is-size-5-mobile is-size-5-tablet is-size-4-widescreen"
-          style={{
-            boxShadow:
-              'rgb(255, 68, 0) 0.5rem 0px 0px, rgb(255, 68, 0) -0.5rem 0px 0px',
-            backgroundColor: 'rgb(255, 68, 0)',
-            color: 'white',
-            lineHeight: '1',
-            padding: '0.25em',
-          }}
-        >
-          {subheading}
-        </h3>
-      </div>
-    </div>
-    <section className="section section--gradient">
+    <WallpaperContainer style={{
+        backgroundImage: `url(${wallpaper})`
+      }}>
+      <FederationsContainer>
+        <ImgFederations src={federations}  />
+      </FederationsContainer>
+      <HeaderMain>
+        <SpanBigSlim>Petit & Blanc</SpanBigSlim><br/>
+        <SpanStyled weight="100">hodowla psów rasowych</SpanStyled><br/>
+        <SpanStyled weight="400" style={{position: 'relative', top: -15}}>bichon frise</SpanStyled>
+      </HeaderMain>
+      <ScrollDown onClick={() => {
+        const windowHeight = window.innerHeight;
+        window.scrollTo({
+          top: windowHeight,
+          behavior: 'smooth'
+        });
+      }}>
+        <a href="#thanks"><span></span></a>
+      </ScrollDown>
+    </WallpaperContainer>
+    <SectionMain>
       <div className="container">
         <div className="section">
           <div className="columns">
@@ -110,7 +231,7 @@ export const IndexPageTemplate = ({
           </div>
         </div>
       </div>
-    </section>
+    </SectionMain>
   </div>
 )
 
