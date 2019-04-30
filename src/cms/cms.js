271a96609d0b56d20f7ff5e53b8ff6a45903718a
Jakub Wojtyra <wojtyraj@gmail.com>
2019-04-30 14:05:59 +0200

    Del cms preview

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,20 +1,20 @@
-import CMS from 'netlify-cms'
-
-import AboutPagePreview from './preview-templates/AboutPagePreview'
-import BlogPostPreview from './preview-templates/BlogPostPreview'
-import ProductPagePreview from './preview-templates/ProductPagePreview'
-import IndexPagePreview from './preview-templates/IndexPagePreview'
-
-import LittersPagePreview from './preview-templates/LittersPagePreview'
-import NewHomePagePreview from './preview-templates/NewHomePagePreview'
-import ExhibitionsPagePreview from './preview-templates/ExhibitionsPagePreview'
-import DogsPagePreview from './preview-templates/DogsPagePreview'
-
-// CMS.registerPreviewTemplate('index', IndexPagePreview)
-// CMS.registerPreviewTemplate('about', AboutPagePreview)
-// CMS.registerPreviewTemplate('products', ProductPagePreview)
-CMS.registerPreviewTemplate('blog', BlogPostPreview)
-CMS.registerPreviewTemplate('litters', LittersPagePreview)
-CMS.registerPreviewTemplate('new-home', NewHomePagePreview)
-CMS.registerPreviewTemplate('exhibitions', ExhibitionsPagePreview)
-CMS.registerPreviewTemplate('dogs', DogsPagePreview)
+// import CMS from 'netlify-cms'
+//
+// import AboutPagePreview from './preview-templates/AboutPagePreview'
+// import BlogPostPreview from './preview-templates/BlogPostPreview'
+// import ProductPagePreview from './preview-templates/ProductPagePreview'
+// import IndexPagePreview from './preview-templates/IndexPagePreview'
+//
+// import LittersPagePreview from './preview-templates/LittersPagePreview'
+// import NewHomePagePreview from './preview-templates/NewHomePagePreview'
+// import ExhibitionsPagePreview from './preview-templates/ExhibitionsPagePreview'
+// import DogsPagePreview from './preview-templates/DogsPagePreview'
+//
+// // CMS.registerPreviewTemplate('index', IndexPagePreview)
+// // CMS.registerPreviewTemplate('about', AboutPagePreview)
+// // CMS.registerPreviewTemplate('products', ProductPagePreview)
+// CMS.registerPreviewTemplate('blog', BlogPostPreview)
+// CMS.registerPreviewTemplate('litters', LittersPagePreview)
+// CMS.registerPreviewTemplate('new-home', NewHomePagePreview)
+// CMS.registerPreviewTemplate('exhibitions', ExhibitionsPagePreview)
+// CMS.registerPreviewTemplate('dogs', DogsPagePreview)
