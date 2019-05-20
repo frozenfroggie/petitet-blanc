062c4e002b126f2a8b0cd3841c83db7ba7c37ccc
Jakub Wojtyra <wojtyraj@gmail.com>
2019-05-20 18:01:33 +0200

    Change homePage fonts weight

--- src/html.js
+++ src/html.js
@@ -12,7 +12,6 @@ export default function HTML(props) {
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
         <link href="https://fonts.googleapis.com/css?family=Fjalla+One" rel="stylesheet"/>
-        <link href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700,900" rel="stylesheet"/>
         {props.headComponents}
       </head>
       <body {...props.bodyAttributes}>
